51c3aa602dd08197849dc28f0e820a953d6849d4
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2020-02-22 23:20:29 -0500

    Add examples

---
+++ examples/mnist/run_cen.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PROBLEM=mnist
+
+CUDA_VISIBLE_DEVICES=0 \
+python -u -m cen.run \
+  experiment=train_eval \
+  problem=${PROBLEM} \
+  encoder=${PROBLEM}/cnn \
+  model=${PROBLEM}/cen_convex \
+  optimizer=adam \
+  run.seed=1
